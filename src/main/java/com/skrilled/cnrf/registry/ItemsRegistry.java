dc296210e7ffa9634ab63f1af5c7e2e9befc8b49
themoon-me <deadangels@foxmail.com>
2025-01-14 04:05:29 +0800

    添加食物特性

--- src/main/java/com/skrilled/cnrf/registry/ItemsRegistry.java
+++ src/main/java/com/skrilled/cnrf/registry/ItemsRegistry.java
@@ -3,6 +3,9 @@ package com.skrilled.cnrf.registry;
 import com.skrilled.cnrf.CNRegionalFood;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -15,8 +18,22 @@ import static com.skrilled.cnrf.CNRegionalFood.MOD_ID;
 
 public enum ItemsRegistry {
 
-    TIANJIN_JIANBING("tianjin_jianbing", () -> new Item(new FabricItemSettings().maxCount(64))),
-    DONGBEI_ZHUOJIAO("dongbei_zhuojiao", () -> new Item(new FabricItemSettings().maxCount(64)));
+    TIANJIN_JIANBING("tianjin_jianbing", () -> new Item(new FabricItemSettings().food(
+                    new FoodComponent.Builder()
+                            .hunger(5)
+                            .saturationModifier(0.4F)
+                            .build())
+            .maxCount(64)
+    )),
+    DONGBEI_ZHUOJIAO("dongbei_zhuojiao", () -> new Item(new FabricItemSettings().food(
+                    new FoodComponent.Builder()
+                            .hunger(2)
+                            .saturationModifier(0.2F)
+                            // 添加反胃效果，概率为20%
+                            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 0), 0.2F)
+                            .build())
+            .maxCount(64)
+    ));
 
 
     private final String name;
