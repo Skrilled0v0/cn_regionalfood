45a356222fa00e17810a06d81d1dfe6ea81dd19c
SkrilledSense <skrilled@aliyun.com>
2025-01-14 01:26:59 +0800

    add baseitem

--- src/main/java/com/skrilled/cnrf/CNRegionalFood.java
+++ src/main/java/com/skrilled/cnrf/CNRegionalFood.java
@@ -20,5 +20,7 @@ public class CNRegionalFood implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		ModItems.init();
+		ModItemGroups.init();
 	}
 }
---
+++ src/main/java/com/skrilled/cnrf/item/TestItem.java
@@ -0,0 +1,9 @@
+package com.skrilled.cnrf.item;
+
+import net.minecraft.item.Item;
+
+public class TestItem extends Item {
+    public TestItem(Settings settings) {
+        super(settings);
+    }
+}
